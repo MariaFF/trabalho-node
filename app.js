265b90494d1b8213eb0f39c59eb5807c4b0533a4
Maria <mariadefatimaf775@gmail.com>
2019-07-22 20:51:38 -0300

    Adicionado cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -16,6 +17,7 @@ const app = express();
 // app.set('view engine', 'jade');
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
